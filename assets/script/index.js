39613a3b30f068eff3e828c0c7335f867b0f4603
Paul Funston <paulfunston9@gmail.com>
2022-11-18 19:58:44 -0600

    added countdown lots off by 1s

--- assets/script/index.js
+++ assets/script/index.js
@@ -17,13 +17,14 @@ function select(selector, parent = document) {
 }
 
 
-const setAlarm = select('.alarm-set');
-const cancelAlarm = select('.alarm-cancel')
-const currentTime = select('.current-time');
+const setAlarmBtn = select('.alarm-set');
+const cancelAlarmBtn = select('.alarm-cancel')
+const currentTimeDisplay = select('.current-time');
 const displayTimer = select('.display-timer');
 const timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]/;
 const userTimeWanted = select('.alarm-time');
 const background = select('body');
+const countdownDisplay = select('.display-countdown');
 
 let now = {};
 let nowYear = 0;
@@ -31,10 +32,13 @@ let nowMonth = 0;
 let nowDay = 0;
 let nowHour = 0;
 let nowMin = 0;
+
+let alarmDate = {};
 let wantedHour = 0;
 let wantedMinute = 0;
-let alarmID = {};
 
+let alarmID = 0;
+let timeRemaining = 0;
 // function on load sets interval
 // function interval runs every second to update time
 
@@ -44,22 +48,22 @@ function startClock() {
 
 function updateTime() {
     getCurrentTime();
-    currentTime.innerHTML = now.toTimeString().substring(0,8);
+    currentTimeDisplay.innerHTML = now.toTimeString().substring(0,8);
+    displayCountdown();
 }
 
 function getCurrentTime() {
-    const date = new Date();
-    now = date;
-    nowYear = date.getFullYear();
-    nowMonth = date.getMonth();
-    nowDay = date.getDate();
-    nowHour = date.getHours();
-    nowMin = date.getMinutes();
+    now = new Date();
+    nowYear = now.getFullYear();
+    nowMonth = now.getMonth();
+    nowDay = now.getDate();
+    nowHour = now.getHours();
+    nowMin = now.getMinutes();
 }
 
-onEvent('onload', currentTime, startClock());
-onEvent('click', setAlarm, setTimer);
-onEvent('click', cancelAlarm, cancelTimer);
+onEvent('onload', currentTimeDisplay, startClock());
+onEvent('click', setAlarmBtn, setTimer);
+onEvent('click', cancelAlarmBtn, cancelTimer);
 
 function setTimer() {
     let alarmWanted = userTimeWanted.value.trim();
@@ -68,20 +72,10 @@ function setTimer() {
         displayAlarm(alarmWanted.substring(0,5));
         let alarmDate = createAlarmDate(alarmWanted);
         armTimer(alarmDate);
+        startCountdown(alarmDate);
     } else {
         displayTimer.innerText = "please enter a valid time";
     }
-}
-
-function cancelTimer() {
-    displayTimer.innerHTML = '';
-    wantedHour = -1;
-    wantedMinute = -1;
-    clearInterval(alarmID);
-    background.classList.remove('alarm');
-    currentTime.classList.remove('alarm');
-    cancelAlarm.classList.remove('alarm');
-    userTimeWanted.value = '';
 }
 
 function displayAlarm(alarmWanted) {
@@ -93,7 +87,8 @@ function createAlarmDate(alarmWanted) {
     wantedMinute = parseInt(alarmWanted.slice(3, 5));
 
     const alarm = new Date(nowYear, nowMonth, nowDay, wantedHour, wantedMinute);
-    if (wantedHour < nowHour || wantedHour === nowHour && wantedMinute < nowMin) {
+    alarmDate = alarm;
+    if (wantedHour < nowHour || (wantedHour === nowHour && wantedMinute < nowMin)) {
         alarm.setDate(alarm.getDate() + 1);
     }
     return alarm;
@@ -101,8 +96,42 @@ function createAlarmDate(alarmWanted) {
 
 function armTimer(alarmDate) {
     let interval = alarmDate - now;
-     if (interval >= 0) 
-        setTimeout(playAlarm, (interval - 3000));
+    setTimeout(playAlarm, (interval - 2000));
+}
+
+function cancelTimer() {
+    displayTimer.innerHTML = '';
+    wantedHour = -1;
+    wantedMinute = -1;
+    clearInterval(alarmID);
+    background.classList.remove('alarm');
+    currentTimeDisplay.classList.remove('alarm');
+    cancelAlarmBtn.classList.remove('alarm');
+    userTimeWanted.value = '';
+    hideCountdown();
+}
+
+// countdown bonus
+function startCountdown(alarmDate) {
+    countdownDisplay.style.visibility = "visible"
+    timeRemaining = alarmDate - now;
+}
+
+function hideCountdown() {
+    countdownDisplay.style.visibility = "hidden"
+}
+
+function displayCountdown() {
+    timeRemaining = alarmDate - now;
+    let hoursRemain = Math.trunc((timeRemaining / 3_600_000)).toString().padStart(2, '0');
+    let minutesRemain = Math.trunc((timeRemaining / 60_000) % 60).toString().padStart(2, '0');
+    let secondsRemain = Math.trunc((timeRemaining / 1000) % 60).toString().padStart(2, '0');
+
+    if (timeRemaining > 0) {
+        countdownDisplay.innerText = `${hoursRemain}:${minutesRemain}:${secondsRemain}`;
+    } else {
+        hideCountdown();
+    }
 }
 
 // Audio
@@ -110,12 +139,18 @@ const alarmBeep = new Audio(`./assets/media/audio/electric-piano-2.wav`);
 alarmBeep.type = 'audio/wav';
 
 function playAlarm() {
+    let count = 0;
     alarmID = setInterval(() => {
         if (wantedHour === nowHour && wantedMinute === nowMin) {
             alarmBeep.play();
-            currentTime.classList.toggle('alarm');
+            currentTimeDisplay.classList.toggle('alarm');
             background.classList.toggle('alarm');
-            cancelAlarm.classList.add('alarm');
+            cancelAlarmBtn.classList.add('alarm');
+        } else {
+            currentTimeDisplay.classList.remove('alarm');
+            background.classList.remove('alarm');
+            cancelAlarmBtn.classList.remove('alarm');
+            count > 15? clearInterval(alarmID): count++;
         }
     }, 1500)
 }
